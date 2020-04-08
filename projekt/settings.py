f2ee2b3269b2d6720268fe5164eabc3532f946ea
barbaraZakrz <34478263+barbaraZakrz@users.noreply.github.com>
2020-04-08 23:22:59 +0200

    rejestracja i logowanie

--- projekt/settings.py
+++ projekt/settings.py
@@ -37,17 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'books',
-    'rest_framework'
+    'APIlend',
+    'rest_framework',
     'rest_framework.authtoken',
 ]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthetication',
+        'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.ISAuthenticated',
+        'rest_framework.permissions.IsAuthenticated',
     )
 }
 
